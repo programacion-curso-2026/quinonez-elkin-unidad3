087b5ea5ae1ddce3d51d0ea7aa3bd52e6bd7849c
elbarreralo27 <elbarreralo@uide.edu.ec>
2026-08-13 20:42:19 -0500

    deber_5_p3_pandas

---
+++ deber_5_p3_pandas/informacion.py
@@ -0,0 +1,16 @@
+vehiculos = {
+    "Marca": ["Toyota", "Chevrolet", "Kia", "Hyundai", "Mazda"],
+    "Modelo": ["Corolla", "Aveo", "Sportage", "Tucson", "3"],
+    "Precio": [23100, 18000, 29000, 32000, 25000]
+}
+
+df_vehiculos = pd.DataFrame(vehiculos)
+
+print("\nINFORMACIÓN DE VEHÍCULOS")
+print(df_vehiculos)
+
+print("\nPrecio promedio:")
+print(df_vehiculos["Precio"].mean())
+
+print("\nVehículos con precio menor a $25000:")
+print(df_vehiculos[df_vehiculos["Precio"] < 25000])
